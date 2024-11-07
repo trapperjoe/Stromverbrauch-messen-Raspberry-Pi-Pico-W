cf4fd4c3a52798e68fe24a6b57b69f85086d059e
Johannes Marko <147028901+trapperjoe@users.noreply.github.com>
2024-11-07 21:49:38 +0100

    Update Test-01.py

--- Test-01.py
+++ Test-01.py
@@ -1,12 +1,16 @@
 # Dieses Programm erwartet einen Impuls auf dem GPIO15 
-# Es reagiert dabei auf jeden Statuswechsel, d.h. sowohl auf eine steigende Flanke, alsauch auf eine fallende Flanke des Impulses 
-# Initial wird der Signalpegel als "LOW" angenommen. Eine steigende Flanke wird dann als ein Wechsel von LOW zu HIGH definiert. 
+# Es reagiert dabei auf jeden Statuswechsel, d.h. sowohl auf eine steigende Flanke,
+# alsauch auf eine fallende Flanke des Impulses 
+# Initial wird der Signalpegel als "LOW" angenommen.
+# Eine steigende Flanke wird dann als ein Wechsel von LOW zu HIGH definiert. 
 # Ebenso ist dann definitionsgemäß eine fallende Flanke ein Wechsel von HIGH auf LOW. 
 # 
-# Während der HIGH Phase des Impulses wird die interne LED eingeschaltet, ansonsten bleibt sie ausgeschaltet.
+# Während der HIGH Phase des Impulses wird die interne LED eingeschaltet,
+# ansonsten bleibt sie ausgeschaltet.
 #
-# Wenn eine steigende Flanke erkannt wurde, dann wird die globale Variable tck1 mit einem Zeitstpempel gesetzt. 
-# Eine fallende Flanke setzt dann die Variable tck2 mit einem weiteren Zeitstempel. 
+# Wenn eine steigende Flanke erkannt wurde, dann wird die globale Variable tck1
+# mit einem Zeitstpempel gesetzt. Eine fallende Flanke setzt dann die Variable tck2
+# mit einem weiteren Zeitstempel. 
 # Die Differenz der beiden Zeitstempel (tck2 - tck1) ist dann die Impulsdauer. 
 # 
 #
@@ -14,40 +18,42 @@
 from machine import Pin
 import utime
 
-# Status-LED onboard
-led_onboard = machine.Pin('LED', machine.Pin.OUT, value=0) 	# Definition der internen LED. 
-								#  ACHTUNG!! Dieser Code gilt nur für einen Raspberry Pico W
+# Status-LED onboard (für Raspberry Pico W)
+led_onboard = machine.Pin('LED', machine.Pin.OUT, value=0)
 
-def Signal_INT(pin):          					# Signal Interrupt Handler
-    global Signal_Status      					# Signal_Status gibt den aktuellen Pegel des Impulses an
-    global tck1, tck2, tck2_v, periode				# globale Variablen für die Zeitermittlung
+# Signal Interrupt Handler
+def Signal_INT(pin): 
+    global Signal_Status 				# Signal_Status gibt den aktuellen Pegel des Impulses an
+    global tck1, tck2, tck2_v, periode	# globale Variablen für die Zeitermittlung
  
-    Signal.irq(handler=None)  					# Interrupt Handler abschalten während der Ausführung 
+    Signal.irq(handler=None)  			# Interrupt Handler abschalten während der Ausführung 
     
-    if (Signal.value() == 1) and (Signal_Status == 0):  	# Signal ist jetzt aktiv (HIGH) war aber vorher auf LOW
-								# Es wurde eine steigende Flanke erkannt !
+    if (Signal.value() == 1) and (Signal_Status == 0):
+    # Signal ist jetzt aktiv (HIGH) war aber vorher auf LOW, d.h. es wurde
+    # eine steigende Flanke erkannt !
 
         Signal_Status = 1     					# Setze Signal_Status auf HIGH
         led_onboard.value(1)    				# Schalte LED_onboard ein
         tck1 = utime.ticks_ms()					# Setze globale Variable tck1 mit einem aktuellen Zeitstempel
         print()
-        print("High tck1 = ", tck1, ' ms')			# Gebe den Wert von tck1 aus
+        print("High tck1 = ", tck1, ' ms')		# Gebe den Wert von tck1 aus
             
-    elif (Signal.value() == 0) and (Signal_Status == 1): 	# Signal ist jetzt nicht aktiv (LOW) war aber vorher auf HIGH
-								# Es wurde eine fallende Flanke erkant !
+    elif (Signal.value() == 0) and (Signal_Status == 1):
+    # Signal ist jetzt nicht aktiv (LOW) war aber vorher auf HIGH, d.h. es wurde
+    # eine fallende Flanke erkannt !
 
         Signal_Status = 0     					# Setze Signal_Status auf LOW
         led_onboard.value(0)    				# Schalte LED_onboard aus
         tck2 = utime.ticks_ms()					# Setze globale Variable tck2 mit einem aktuellen Zeitstempel
-        print("Low  tck2 = ", tck2, ' ms')			# Gebe den Wert von tck2 aus
+        print("Low  tck2 = ", tck2, ' ms')		# Gebe den Wert von tck2 aus
         
         ## print("tck2_v = \t", tck2_v, '\t', "tck2 = \t", tck2)
-        if tck2_v > 0:						# tck2_v ist der vorherige Wert von tck2 beim letzten Aufruf
-            periode = tck2 - tck2_v				#  periode gibt die Zeitdifferenz zwischen zwei fallenden Flanken an
+        if tck2_v > 0:				# tck2_v ist der vorherige Wert von tck2 beim letzten Aufruf
+            periode = tck2 - tck2_v	#  periode gibt die Zeitdifferenz zwischen zwei fallenden Flanken an
             ## print("periode = ", periode, 'ms')
-        tck2_v = tck2    					# Setze die Variable tck2_v auf den Wert von tck2 für den nächsten Aufruf
+        tck2_v = tck2    			# Setze die Variable tck2_v auf den Wert von tck2 für den nächsten Aufruf
     
-    Signal.irq(handler=Signal_INT)				# Interrupt Handler wieder einschalten
+    Signal.irq(handler=Signal_INT)	# Interrupt Handler wieder einschalten
     
 
 # Definiere ein "Objekt" für den Statuswechsel des Signals
@@ -60,17 +66,18 @@ Signal.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=Sig
 # Start des Hauptprogramms
 
 # Setze die Status Variable für das Impulssignal
-Signal_Status = Signal.value() 					# Signal_Status sollte anfänglich auf LOW gesetzt sein
-								# wegen dem internen PULLDOWN Befehl auf GPIO15 
-print("Signal Status: ", Signal_Status)				# Gib zur Überprüfung den initialen Zustand von Signal_Status aus
+Signal_Status = Signal.value() 			# Signal_Status sollte anfänglich auf LOW gesetzt sein
+                                        # wegen dem internen PULLDOWN Befehl auf GPIO15 
+print("Signal Status: ", Signal_Status)	# Gib zur Überprüfung den initialen Zustand von Signal_Status aus
 
 
-counter = 0							# Setze den Schleifenzähler initial auf 0
-tck1 = 0; tck2 = 0; tck2_v = -1; periode = -1			# Setze die globalen Variablen tck2_v und periode auf ungültige Werte, 
-								# da diese erst später im Signal Interrupt Handler gesetzt werden
+counter = 0								# Setze den Schleifenzähler initial auf 0
+tck1 = 0; tck2 = 0; tck2_v = -1; periode = -1 # Setze die globalen Variablen tck2_v und periode auf ungültige Werte, 
+                                        # da diese erst später im Signal Interrupt Handler gesetzt werden
 print("Start...... !")
-while True:							# Endlos Scheife
-    print("counter = ", counter, "\t Pulsdauer = ", tck2-tck1, 'ms') # Gib den Schleifenzähler (couter) und die Impulsdauer aus
+while True:								# Endlos Scheife
+    # Gib den Schleifenzähler (counter) und die Impulsdauer aus
+    print("counter = ", counter, "\t Pulsdauer = ", tck2-tck1, 'ms')
     utime.sleep(2)						# Warte ein wenig....
     counter += 1						# Erhöhe den Schleifenzähler
         
@@ -78,3 +85,4 @@ while True:							# Endlos Scheife
 #    
 #
 #######################################################################################
+
